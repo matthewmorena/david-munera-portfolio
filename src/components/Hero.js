a6de358a61e4b33c8a8f3afb0c847725040edc00
Matthew Morena <memorena6@gmail.com>
2024-11-17 16:34:27 -0500

    homepage and hero tweaks

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,21 @@
 import './Hero.css';
 
-const Hero = ({ imageSrc, altText, children }) => {
+const Hero = ({ imageSrc, altText, children, imageOnRight = false }) => {
     return (
         <div className="hero">
+            {!imageOnRight && (
+                <div className="hero-image">
+                    <img alt={altText} src={imageSrc}></img>
+                </div>
+            )}
             <div className="hero-text">
                 {children} {/* Render the passed HTML/JSX */}
             </div>
-            <div className="hero-image">
-                <img alt={altText} src={imageSrc}></img>
-            </div>
+            {imageOnRight && (
+                <div className="hero-image">
+                    <img alt={altText} src={imageSrc}></img>
+                </div>
+            )}
         </div>
     );
 }
