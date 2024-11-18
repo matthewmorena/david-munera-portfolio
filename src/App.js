9bb4d06462d3d1fb9f27656a59bdd52a28c696ca
Matthew Morena <memorena6@gmail.com>
2024-11-17 19:21:28 -0500

    Footer

--- src/App.js
+++ src/App.js
@@ -80,12 +80,26 @@ function App() {
         </div>
       </Hero>
 
-      <hr id="projects"></hr>
+      <hr id="contact"></hr>
       <Banner
         title="Contact Me"
         subtitle="Let my creativity and expertise transform your vision into a stunning, user-friendly website or app that stands out and drives results."
         text="Contact me through my Email or LinkedIn Below!"
       ></Banner>
+
+      <div style={{width: "10%", display: "flex", justifyContent: "space-between", margin: "auto"}}>
+        <a href="https://www.linkedin.com/in/david-munera-a2862a1b6/">
+          <img alt="linkedin logo" src="https://static.wixstatic.com/media/11062b_60c5fc4a3ecd49f2a697206b09eeace1~mv2.png/v1/fill/w_53,h_53,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_60c5fc4a3ecd49f2a697206b09eeace1~mv2.png"></img>
+        </a>
+        <a href="mailto:davidmunera828@gmail.com">
+          <img alt="email logo" src="https://static.wixstatic.com/media/e645a6_0230c684ae3e436ca3e1b7757f28ff96~mv2.jpg/v1/fill/w_53,h_53,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/e645a6_0230c684ae3e436ca3e1b7757f28ff96~mv2.jpg"></img>
+        </a>
+      </div>
+
+      <p style={{marginTop: "50px"}}>© David Munera Designs 2024&nbsp;&nbsp;&nbsp;
+        <a style={{textDecoration: "none", color: "#000"}} href="mailto:davidmunera828@gmail.com">Email</a>&nbsp;&nbsp;&nbsp;
+        <a style={{textDecoration: "none", color: "#000"}} href="https://www.linkedin.com/in/david-munera-a2862a1b6/">LinkedIn</a>
+      </p>
 
     </div>
   );
